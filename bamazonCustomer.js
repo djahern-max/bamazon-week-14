b4d1fec1a0acffbcafff33dff7bbc14331847b00
Dano <danielaherniv@gmail.com>
2019-09-22 13:32:25 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,6 +19,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id" + connection.threadId);
+    readProducts();
 });
 
 function readProducts() {
@@ -34,11 +35,80 @@ function readProducts() {
             displayTable.push(
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             )
-
         }
-
         console.log(displayTable.toString());
-        connection.end();
+        // connection.end();
+        purchaseRequest();
     })
 }
-readProducts();
+
+
+function purchaseRequest() {
+
+    inquirer
+        .prompt([{
+                name: "ID",
+                type: "input",
+                message: "Which item would you like to purchase?",
+                filter: Number
+            },
+            {
+                name: "Quantity",
+                type: "input",
+                message: "How many items do you wish to purchase?",
+                filter: Number
+            },
+        ]).then(function(answers) {
+            let test = answers;
+            let purchaseID = answers.ID;
+            let quantityToBuy = answers.Quantity;
+            purchaseOrder(purchaseID, quantityToBuy)
+        });
+};
+
+function purchaseOrder(ID, amtNeeded) {
+
+    connection.query("Select * FROM products WHERE item_id = " + ID, function(err, res) {
+        // console.log(res);
+
+        if (err) {
+            console.log(err)
+        };
+        if (amtNeeded <= res[0].stock_quantity) {
+            let totalCost = res[0].price * amtNeeded;
+            console.log("You slected the following product:  " + res[0].product_name + "\n" + "We have your item in stock \n" + "your total cost for " + amtNeeded + " units is " + totalCost);
+            let quantityBeforeSale = res[0].stock_quantity;
+            console.log("Units available before sale: " + quantityBeforeSale);
+            let quantityAfterSale = quantityBeforeSale - amtNeeded;
+            console.log("Units available after sale: " + quantityAfterSale);
+            let itemID = res[0].item_id;
+            console.log("Item Sold: Item ID " + itemID);
+            updateProduct();
+
+            function updateProduct() {
+                console.log("View product database for updated quantities following sale.....\n");
+
+                var query = connection.query(
+                    "UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: quantityAfterSale
+                        },
+                        {
+                            item_id: itemID
+                        }
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                        // console.log(res.affectedRows + " products updated!\n");
+
+                    }
+                );
+
+            }
+
+        } else {
+            console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "s" + " to complete this order.");
+        };
+
+    })
+
+}
