c336ddd8fcfc3dd13f92cb7dc51fc00a1a45435d
Dane Ahern <danielaherniv@gmail.com>
2019-09-23 15:29:42 -0400

    Add bamazonManager2.js

---
+++ bamazonManager2.js
@@ -0,0 +1,85 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "CWkeller1234!",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+});
+
+function displayInventory() {
+    connection.query('SELECT * FROM Products', function (err, res) {
+        if (err) {
+            console.log(err)
+        };
+        var theDisplayTable = new Table({
+            head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity'],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (i = 0; i < res.length; i++) {
+            theDisplayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(theDisplayTable.toString());
+        choices();
+
+    });
+};
+
+function lessThanFive() {
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        for (let i = 0; i < res.length; i++) {
+            // console.log(res[i].stock_quantity);
+            if (res[i].stock_quantity <= 5) {
+                console.log("The following products have low inventory: " + res[i].product_name + "\n" + "Product # = " + res[i].item_id + "\n" + "Units in Inventory: " + res[i].stock_quantity);
+
+            }
+        }
+    })
+}
+
+function choices() {
+
+    inquirer.prompt([{
+        name: "action",
+        type: "list",
+        message: "Please select from the following list",
+        choices: ["View Products", "View Low Inventory", "Add to Low Inventory", "Add a New Product"]
+    }]).then(function (answers) {
+        switch (answers.action) {
+            case "View Products":
+                displayInventory();
+                break;
+            case "View Low Inventory":
+                lessThanFive();
+                break;
+            case "Add to Low Inventory":
+                // inventoryAdjustment();
+                break;
+            case "Add a New Product":
+                //Function
+                break;
+
+        }
+    })
+}
+
+choices();
